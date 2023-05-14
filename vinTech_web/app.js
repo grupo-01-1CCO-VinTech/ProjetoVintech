8e5927c330de3bf90480e22704199694545c8c3d
rafaelSchimas <rafael.ranaudo@sptech.school>
2023-05-14 20:05:30 -0300

    Arrumando pastas

--- vinTech_web/app.js
+++ vinTech_web/app.js
@@ -1,7 +1,7 @@
 process.env.AMBIENTE_PROCESSO = "desenvolvimento";
 // process.env.AMBIENTE_PROCESSO = "producao";
 
-const express = require("express");
+var express = require("express");
 var cors = require("cors");
 var path = require("path");
 var PORTA = process.env.AMBIENTE_PROCESSO == "desenvolvimento" ? 3333 : 8080;
