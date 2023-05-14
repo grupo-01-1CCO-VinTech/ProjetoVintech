7f0a161f0c7026fe277b99fe14bd7199e562f210
rafaelSchimas <rafael.ranaudo@sptech.school>
2023-05-14 19:55:32 -0300

    Insert do usuarioModel adaptado para nosso banco

--- vinTech_web/src/models/usuarioModel.js
+++ vinTech_web/src/models/usuarioModel.js
@@ -19,13 +19,16 @@ function entrar(email, senha) {
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucao
-function cadastrar(nome, email, senha, cpf) {
+function cadastrar(cnpj, nomeEmpresa, nomeFantasia, CEP, logradouro, numeroEmpresa, baiorroEmpresa, 
+    cidadeEmpresa, UFEmpresa, telefoneEmpresa, telefoneSecundarioEmpresa, emailEmpresa) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucao = `
-        INSERT INTO usuario (nome, email, senha, cpf) VALUES ('${nome}', '${email}', '${senha}', '${cpf}');
+        INSERT INTO usuario (cnpj, nomeEmpresa, nomeFantasia, CEP, logradouro, numeroEmpresa, baiorroEmpresa, 
+            cidadeEmpresa, UFEmpresa, telefoneEmpresa, telefoneSecundarioEmpresa, emailEmpresa) VALUES ('${cnpj}','${nomeEmpresa}','${nomeFantasia}','${CEP}',
+            '${logradouro}','${numeroEmpresa}','${baiorroEmpresa}','${cidadeEmpresa}','${UFEmpresa}','${telefoneEmpresa}','${telefoneSecundarioEmpresa}','${emailEmpresa}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
