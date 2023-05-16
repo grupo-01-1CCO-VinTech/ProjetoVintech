8c1d52f2306f0005d1c4bb7a39ded373fc279e0d
Julia Justino <stephany.justino@sptech.school>
2023-05-15 22:03:36 -0300

    atualizei as variáveis

--- vinTech_web/src/controllers/usuarioController.js
+++ vinTech_web/src/controllers/usuarioController.js
@@ -33,7 +33,7 @@ function entrar(req, res) {
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está indefinida!");
     } else {
-        
+
         usuarioModel.entrar(email, senha)
             .then(
                 function (resultado) {
@@ -62,30 +62,113 @@ function entrar(req, res) {
 
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-    var nome = req.body.nomeServer;
-    var email = req.body.emailServer;
+
+    /*Variveis com os dados do cad*/ 
+    var nome = req.body.nomeEmpresaServer;
+    var nomeFantasia = req.body.nomeFantasiaServer;
+    var cnpj = req.body.cnpjServer;
+    var cep = req.body.cepServer;
+    var logradouro = req.body.logradouroServer;
+    var numero = req.body.numeroServer;
+    var bairro = req.body.bairroServer;
+    var cidade = req.body.cidadeServer;
+    var uf = req.body.ufServer;
+    var telefone = req.body.telefoneServer;
+    var telefoneSecundario = req.body.telefoneSecundarioServer;
+    var email = req.body.emailEmpresaServer;
+    var admEmail = req.body.emailAdmServer;
+    var loginAdm = req.body.loginServer;
     var senha = req.body.senhaServer;
-    var cpf = req.body.cpfServer;
+
+    /*
+                  Dicionario das variaveis
+                nomeEmpresaServer: empresa.dados.nome,
+                nomeFantasiaServer: empresa.dados.nomeFantasia,
+                cnpjServer: empresa.dados.cnpj,
+                cepServer: empresa.endereco.cep,
+                logradouroServer: empresa.endereco.logradouro,
+                numeroServer: empresa.endereco.numero,
+                bairroServer: empresa.endereco.bairro,
+                cidadeServer: empresa.endereco.cidade,
+                ufServer: empresa.endereco.uf,
+                telefoneServer: empresa.contato.telefone,
+                telefoneSecundarioServer: empresa.contato.telefoneSecundario,
+                emailEmpresaServer: empresa.contato.email,
+                emailAdmServer: empresa.adm.email,
+                loginServer: empresa.adm.login,
+                senhaServer: empresa.adm.senha
+    */
+
 
     // Faça as validações dos valores
-    if (nome == undefined) {
+    /*Vericações dos campos, em base com o código da CadastroEmpresa.html */
+    var erro = false
+    if (loginServer.length < 8) {
+        erro = true
+        res.status(400).send("O campo Login deve ter pelo menos 8 caracteres!");
+    }
+    if (emailAdmServer == "" || emailAdmServer.indexOf('@') == -1 ||
+        emailAdmServer.indexOf('.') == -1 || emailAdmServer.length < 10) {
+            erro = true
+        res.status(400).send("O campo E-mail deve ter um formato válido!");
+    }
+    if (senhaServer.length < 8) {
+        erro = true
+        res.status(400).send("O campo Senha deve ter pelo menos 8 caracteres!");
+    }
+    if (confirmarSenha != senhaServer) {
+        erro = true
+        res.status(400).send("As senhas não conferem!");
+    }
+
+    if (erro == false) {
+        cadastrar()
+    } else {
+        res.status(400).send("Alerta! Campos inválidos encontrados, verifique o conteúdo!")
+    }
+
+    /* Os campos do model de acordo com as variaveis */
+    usuarioModel.cadastrar(nome, nomeFantasia, cnpj, cep, logradouro, numero, bairro, cidade, uf, telefone, telefoneSecundario, email, admEmail, loginAdm, senha)
+    .then(
+        function (resultado) {
+            res.json(resultado);
+        }
+    ).catch(
+        /*Erro caso consulta */
+        function (erro) {
+            console.log(erro);
+            console.log(
+                "\nHouve um erro ao realizar o cadastro! Erro: ",
+                erro.sqlMessage
+            );
+            res.status(500).json(erro.sqlMessage);
+        }
+    );
+}
+
+
+
+
+
+  /*  Código da Júlia
+   if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
-    } else if(nome.length <= 1){
+    } else if (nome.length <= 1) {
         res.status(400).send("Seu nome está muito pequeno!");
-    } else if(email.indexOf('@') == -1 || email.indexOf('.') == -1){
+    } else if (email.indexOf('@') == -1 || email.indexOf('.') == -1) {
         res.status(400).send("Seu email não é válido!");
-    } else if(senha.length <= 5){
+    } else if (senha.length <= 5) {
         res.status(400).send("Sua senha é muito fraca!");
-    } else if(cpf.length < 14 || cpf.length >= 15){
+    } else if (cpf.length < 14 || cpf.length >= 15) {
         res.status(400).send("Seu CPF não é válido!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
     } else {
-        
+ */
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, cpf)
+       /*  usuarioModel.cadastrar(nome, email, senha, cpf)
             .then(
                 function (resultado) {
                     res.json(resultado);
@@ -100,8 +183,8 @@ function cadastrar(req, res) {
                     res.status(500).json(erro.sqlMessage);
                 }
             );
-    }
-}
+    } */
+
 
 module.exports = {
     entrar,
