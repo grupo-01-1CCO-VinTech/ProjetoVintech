8efc8e7a8bd226d58b5970a128d8db63241b17ac
Davi-Hilario <davi.hilario@sptech.school>
2023-06-05 14:26:46 -0300

    consulta de plantações no select

--- vinTech_arquiteturaProduto/API_marise/main.js
+++ vinTech_arquiteturaProduto/API_marise/main.js
@@ -91,13 +91,13 @@ const serial = async (
                 // 'INSERT INTO sensores (dht11_umidade, dht11_temperatura, luminosidade, lm35_temperatura, chave) VALUES (?, ?, ?, ?, ?)',
                 // [dht11Umidade, dht11Temperatura, luminosidade, lm35Temperatura, chave]
                 'INSERT INTO Registro (temperaturaRegistro, umidadeRegistro, dataRegistro, fkSensor) VALUES (?, ?, now() ,?)',
-                [variarValores(dht11Temperatura) - 8, variarValores(dht11Umidade) - 20, 1]
+                [variarValores(dht11Temperatura) - 10, variarValores(dht11Umidade) - 20, 1]
             );
             await poolBancoDados.execute(
                 // 'INSERT INTO sensores (dht11_umidade, dht11_temperatura, luminosidade, lm35_temperatura, chave) VALUES (?, ?, ?, ?, ?)',
                 // [dht11Umidade, dht11Temperatura, luminosidade, lm35Temperatura, chave]
                 'INSERT INTO Registro (temperaturaRegistro, umidadeRegistro, dataRegistro, fkSensor) VALUES (?, ?, now() ,?)',
-                [dht11Temperatura, dht11Umidade, 2]
+                [variarValores(dht11Temperatura) - 5, variarValores(dht11Umidade) - 20, 2]
             );
             await poolBancoDados.execute(
                 // 'INSERT INTO sensores (dht11_umidade, dht11_temperatura, luminosidade, lm35_temperatura, chave) VALUES (?, ?, ?, ?, ?)',
